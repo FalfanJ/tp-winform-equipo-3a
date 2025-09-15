84229f39ef5f7cc6908c9a7e0ede5ce37a642380
DelfinaHache <delfihiriart@gmail.com>
2025-09-14 22:42:24 -0300

    interfaz gráfica de los métodos de marca

---
+++ TPWinForm/FormMarcas.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace TPWinForm
+{
+    partial class FormMarcas
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridViewMarcas;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtNuevaMarca;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnDetalle;
+        private System.Windows.Forms.Button btnActualizar;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridViewMarcas = new System.Windows.Forms.DataGridView();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtNuevaMarca = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnDetalle = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMarcas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewMarcas
+            // 
+            this.dataGridViewMarcas.AllowUserToAddRows = false;
+            this.dataGridViewMarcas.AllowUserToDeleteRows = false;
+            this.dataGridViewMarcas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewMarcas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMarcas.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewMarcas.MultiSelect = false;
+            this.dataGridViewMarcas.Name = "dataGridViewMarcas";
+            this.dataGridViewMarcas.ReadOnly = true;
+            this.dataGridViewMarcas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewMarcas.Size = new System.Drawing.Size(500, 200);
+            this.dataGridViewMarcas.TabIndex = 0;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(12, 230);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(200, 20);
+            this.txtBuscar.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(230, 228);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtNuevaMarca
+            // 
+            this.txtNuevaMarca.Location = new System.Drawing.Point(12, 270);
+            this.txtNuevaMarca.Name = "txtNuevaMarca";
+            this.txtNuevaMarca.Size = new System.Drawing.Size(200, 20);
+            this.txtNuevaMarca.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(230, 268);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(320, 268);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 5;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(410, 268);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnDetalle
+            // 
+            this.btnDetalle.Location = new System.Drawing.Point(500, 268);
+            this.btnDetalle.Name = "btnDetalle";
+            this.btnDetalle.Size = new System.Drawing.Size(75, 23);
+            this.btnDetalle.TabIndex = 7;
+            this.btnDetalle.Text = "Detalle";
+            this.btnDetalle.UseVisualStyleBackColor = true;
+            this.btnDetalle.Click += new System.EventHandler(this.btnDetalle_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(500, 228);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 8;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // FormMarcas
+            // 
+            this.ClientSize = new System.Drawing.Size(600, 320);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnDetalle);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNuevaMarca);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.dataGridViewMarcas);
+            this.Name = "FormMarcas";
+            this.Text = "Gestión de Marcas";
+            this.Load += new System.EventHandler(this.FormMarcas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMarcas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ TPWinForm/FormMarcas.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Negocio;
+using Dominio;
+
+namespace TPWinForm
+{
+    public partial class FormMarcas : Form
+    {
+        private MarcasNegocio negocio;
+
+        public FormMarcas()
+        {
+            InitializeComponent();
+            negocio = new MarcasNegocio();
+        }
+
+        private void FormMarcas_Load(object sender, EventArgs e)
+        {
+            CargarMarcas();
+        }
+
+        private void CargarMarcas()
+        {
+            dataGridViewMarcas.DataSource = null;
+            dataGridViewMarcas.DataSource = negocio.Listar();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string criterio = txtBuscar.Text.Trim();
+            dataGridViewMarcas.DataSource = negocio.Buscar(criterio);
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNuevaMarca.Text))
+            {
+                MessageBox.Show("Ingrese un nombre de marca.");
+                return;
+            }
+
+            var nueva = new Marcas { Marca = txtNuevaMarca.Text.Trim() };
+            negocio.Agregar(nueva);
+            CargarMarcas();
+            txtNuevaMarca.Clear();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewMarcas.CurrentRow != null)
+            {
+                var seleccionada = (Marcas)dataGridViewMarcas.CurrentRow.DataBoundItem;
+                if (string.IsNullOrWhiteSpace(txtNuevaMarca.Text))
+                {
+                    MessageBox.Show("Ingrese el nuevo nombre para modificar la marca.");
+                    return;
+                }
+                seleccionada.Marca = txtNuevaMarca.Text.Trim();
+                negocio.Modificar(seleccionada);
+                CargarMarcas();
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewMarcas.CurrentRow != null)
+            {
+                var seleccionada = (Marcas)dataGridViewMarcas.CurrentRow.DataBoundItem;
+                var confirm = MessageBox.Show($"¿Está seguro de eliminar la marca {seleccionada.Marca}?",
+                                              "Confirmar eliminación",
+                                              MessageBoxButtons.YesNo,
+                                              MessageBoxIcon.Warning);
+                if (confirm == DialogResult.Yes)
+                {
+                    negocio.Eliminar(seleccionada.Id);
+                    CargarMarcas();
+                }
+            }
+        }
+
+        private void btnDetalle_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewMarcas.CurrentRow != null)
+            {
+                var seleccionada = (Marcas)dataGridViewMarcas.CurrentRow.DataBoundItem;
+                var detalle = negocio.ObtenerPorId(seleccionada.Id);
+                MessageBox.Show($"ID: {detalle.Id}\nMarca: {detalle.Marca}", "Detalle de Marca");
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            CargarMarcas();
+        }
+    }
+}
